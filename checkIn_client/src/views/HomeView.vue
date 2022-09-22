59541b84a89b926a8b9bfd923e29f90c13d19627
codeblack-19 <codeblack-19@users.noreply.github.com>
2022-09-22 11:52:58 +0000

    clean-up

--- checkIn_client/src/views/HomeView.vue
+++ checkIn_client/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>hello world</h2>
 </template>
+
+<script setup>
+
+</script>
